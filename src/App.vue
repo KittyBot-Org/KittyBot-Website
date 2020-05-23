54870cd76ae4b970a919c9297afd2f55aa40b239
ToπSenpai <tobi@strasserhof.com>
2020-05-24 01:14:44 +0200

    added first layout & dockerfile

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <div id="app-nav">
+      <router-link to="/">Home</router-link>
+      <a href="/#features">Features</a>
+      <a href="/#commands">Commands</a>
       <router-link to="/about">About</router-link>
+      <a class="login" href="/login">login with Discord</a>
     </div>
     <router-view />
   </div>
 </template>
 
-<style>
+<style lang="less">
+@import "./style/style.less";
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  .font-default;
+  background-color: @secondary;
   text-align: center;
-  color: #2c3e50;
+  .fc;
+  min-height: 100%;
+  &-nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 12px;
+    & * {
+      margin-left: 4px;
+      margin-right: 4px;
+    }
+  }
 }
 
-#nav {
-  padding: 30px;
+a {
+  text-decoration: none;
+  .fc;
+  &:hover {
+    .fc-dark;
+  }
+  &.router-link-exact-active {
+    color: @primary;
+  }
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.login {
+  border: 4px solid @primary;
+  border-radius: 32px;
+  padding: 8px;
+  color: @primary;
+  &:hover {
+    color: @primary-dark;
+    border-color: @primary-dark;
+  }
 }
 </style>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,11 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../../assets/logo.png" />
+  <div class="view-home">
+    <div class="view-home-title">
+      <span class="title">KittyBot</span>
+      <img alt="Vue logo" src="../../assets/KittyBlink.gif" />
+      <span class="subtitle">Here to manage your Discord</span>
+    </div>
+    <div id="features">
+      <span>Features</span>
+    </div>
+    <div id="commands">
+      <span>Commands</span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "ViewHome"
 };
 </script>
+
+<style lang="less" scoped>
+@import "../../style/style.less";
+
+.view-home-title {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.title {
+  .font-bungee;
+  color: @primary;
+  font-weight: bolder;
+  font-size: 64px;
+}
+.subtitle {
+  .font-bangers;
+  font-weight: bolder;
+  font-size: 42px;
+}
+</style>
