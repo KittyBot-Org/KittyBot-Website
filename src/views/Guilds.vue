758ea0f127ebc92ea8fafd1ce33e212a49f83ba3
TopiSenpai <tobi@strasserhof.com>
2020-10-13 15:17:55 +0200

    wip sync to pc

--- src/views/Guilds.vue
+++ src/views/Guilds.vue
@@ -10,26 +10,28 @@
       />
     </div>
     <div
-      v-else-if="guilds.length > 0"
-      class="view-guilds-guilds"
+      v-else-if="guilds.length == 0"
+      class="view-guilds-guilds-no-guilds"
       :class="{ mobile: isMobile }"
     >
-      <router-link
-        v-for="guild in guilds"
-        :key="guild.id"
-        :to="`/guilds/${guild.id}/dashboard`"
-      >
-        <guild-icon :icon="guild.icon" :text="guild.name" :alt="guild.name" />
-        <span>{{ guild.name }}</span>
-      </router-link>
-    </div>
-    <div v-else class="view-guilds-no-guilds">
       <h2>
         I'm not in any mutual servers in which you have Administrator permission
       </h2>
       <h2>Do you want to add me to your server?</h2>
       <invite-button />
-    </div>
+  </div>
+   <v-simple-table v-else>
+     <template v-slot:default>
+       <tbody>
+        <tr>
+          <td><guild-icon :icon="guild.icon" :text="guild.name" :alt="guild.name" /></td>
+          <td><span>{{ guild.name }}</span></td>
+          <td><v-btn :to="`/guilds/${guild.id}/dashboard`" color="primary">Open Dashboard</v-btn></td>
+        </tr>
+       </tbody>
+     </template>
+   </v-simple-table> 
+
   </div>
 </template>
 
