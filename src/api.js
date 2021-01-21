b8dc90246ac4fb4dc03980b4fbe7d9a46907e86a
TopiSenpai <tobi@strasserhof.com>
2021-01-21 01:41:04 +0100

    fixed bruh

--- src/api.js
+++ src/api.js
@@ -3,8 +3,6 @@ import Vue from "vue";
 const BACKEND_URL =
   process.env.NODE_ENV === "production"
     ? "https://api-dev.kittybot.de/"
-    : process.env.VUE_APP_DOCKER_ENV
-    ? "https://api.localhost/"
     : "http://localhost:6969/";
 const CLIENT_ID =
   process.env.NODE_ENV === "production"
